32a0d41ad08a8d93b812dd902a3002b6214573cd
Philip Ainza <philainza@gmail.com>
2016-04-20 08:44:14 -0700

    Add validations

--- app/models/open_mat.rb
+++ app/models/open_mat.rb
@@ -1,4 +1,14 @@
 class OpenMat < ActiveRecord::Base
+
+  validates :location, :host, :starts_at, presence: true
+
+  validates :number_people_attending, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :image_file_name, allow_blank: true, format: {
+    with:    /\w+\.(gif|jpg|png)\z/i,
+    message: "must reference a GIF, JPG, or PNG image"
+  }
+
   def expired?
     starts_at < Time.now
   end
